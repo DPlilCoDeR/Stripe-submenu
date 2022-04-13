0681c9b72f50a0319a4eb107b34326c55448bef0
Jason Nicolas Carvajal Perez <doble_papi@MacBook-Pro.local>
2022-04-13 23:09:47 +0200

    create hook GlobalContext

--- src/context.js
+++ src/context.js
@@ -1,2 +1,43 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from "react";
+import sublinks from "./data";
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+
+  function openSidebar() {
+    setIsSidebarOpen(true);
+  }
+
+  function closeSidebar() {
+    setIsSidebarOpen(false);
+  }
+
+  function openSubmenu() {
+    setIsSubmenuOpen(true);
+  }
+
+  function closeSubmenu() {
+    setIsSubmenuOpen(false);
+  }
+  return (
+    <AppContext.Provider
+      value={
+        (isSidebarOpen,
+        isSubmenuOpen,
+        openSidebar,
+        closeSidebar,
+        openSubmenu,
+        closeSubmenu)
+      }
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const GlobalContext = () => {
+  return useContext(AppContext);
+};
