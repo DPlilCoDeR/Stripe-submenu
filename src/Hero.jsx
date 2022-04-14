5fa5df92bbd6610098a93ca42be2914f81d5afa6
Jason Nicolas Carvajal Perez <doble_papi@MacBook-Pro.local>
2022-04-14 02:02:07 +0200

    Hero  component

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
-import phoneImg from './images/phone.svg'
+import React from "react";
+import phoneImg from "./images/phone.svg";
 
 const Hero = () => {
-  return <h2>hero component</h2>
-}
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Infraestructura de pagos para Internet</h1>
+          <p>
+            Millones de empresas de todos los tamaños, desde startups hasta
+            grandes corporaciones, usan el software y las API de Stripe para
+            aceptar pagos, hacer transferencias y gestionar sus negocios en
+            Internet.
+          </p>
+          <button className="btn">Start now</button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} className="phone-img" alt="phone" />
+        </article>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
