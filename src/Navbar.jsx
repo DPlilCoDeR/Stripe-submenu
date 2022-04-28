7897ea65d8ed41b315bdc2422828f8cae775dfa9
Jason Nicolas Carvajal Perez <doble_papi@MacBook-Pro.local>
2022-04-28 17:17:05 +0200

    refactor NavBar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,35 +1,35 @@
-import React from "react";
-import logo from "./images/logo.svg";
-import { FaBars } from "react-icons/fa";
-import { GlobalContext } from "./context";
+import React from 'react'
+import logo from './images/logo.svg'
+import { FaBars } from 'react-icons/fa'
+import { GlobalContext } from './context'
 
 const Navbar = () => {
-  const { openSidebar, openSubmenu, closeSubmenu } = GlobalContext();
+  const { openSidebar } = GlobalContext()
 
   return (
     <nav>
-      <div className="nav-center">
-        <div className="nav-header">
-          <img src={logo} alt="stripe-logo" className="nav-logo" />
-          <button className="btn toggle-btn" onClick={openSidebar}>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} alt='stripe-logo' className='nav-logo' />
+          <button className='btn toggle-btn' onClick={openSidebar}>
             <FaBars />
           </button>
         </div>
-        <ul className="nav-links">
+        <ul className='nav-links'>
           <li>
-            <button className="link-btn">products</button>
+            <button className='link-btn'>products</button>
           </li>
           <li>
-            <button className="link-btn">developers</button>
+            <button className='link-btn'>developers</button>
           </li>
           <li>
-            <button className="link-btn">company</button>
+            <button className='link-btn'>company</button>
           </li>
         </ul>
-        <button className="btn signin-btn">Sign in</button>
+        <button className='btn signin-btn'>Sign in</button>
       </div>
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
