ab1517d748b8aec56659667112659f068bc69251
Jason Nicolas Carvajal Perez <doble_papi@MacBook-Pro.local>
2022-05-09 06:20:12 +0200

    Sidemenu looks great

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -4,41 +4,42 @@ import sublinks from './data'
 import { GlobalContext } from './context'
 
 const Sidebar = () => {
-  const { closeSidebar, isSidebarOpen, isSubmenuOpen } = GlobalContext()
-  console.log(isSidebarOpen, isSubmenuOpen)
+  const { closeSidebar, isSidebarOpen } = GlobalContext()
 
   return (
-    <aside
+    <div
       className={`${
         isSidebarOpen ? 'sidebar-wrapper show' : 'sidebar-wrapper'
       }`}
     >
-      <div className='sidebar'>
+      <aside className='sidebar'>
         <button className='close-btn' onClick={closeSidebar}>
           <FaTimes />
         </button>
-      </div>
 
-      <div className='sidebar-links'>
-        {sublinks.map((sublinks, index) => {
-          const { page, links } = sublinks
-          return (
-            (<h4>{page}</h4>),
-            links.map((link, index) => {
-              return (
-                <ul key={index}>
-                  <li>{link.label}</li>
-                  <li>{link.icon}</li>
-                  <li>
-                    <a href={link.url}>{link.label}</a>
-                  </li>
-                </ul>
-              )
-            })
-          )
-        })}
-      </div>
-    </aside>
+        <div className='sidebar-links'>
+          {sublinks.map((item, index) => {
+            const { page, links } = item
+            return (
+              <article key={index}>
+                <h4>{page}</h4>
+                <div className='sidebar-sublinks'>
+                  {links.map((link, index) => {
+                    const { url, icon, label } = link
+                    return (
+                      <a key={index} href={url}>
+                        {icon}
+                        {label}
+                      </a>
+                    )
+                  })}
+                </div>
+              </article>
+            )
+          })}
+        </div>
+      </aside>
+    </div>
   )
 }
 
